339cb33d045734e8f2b36400dc2dd905d0e23b05
Sophia Jin <sjschool26@gmail.com>
2026-03-27 13:01:10 -0400

    prints data

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         /* testing out Matrix class
@@ -11,5 +15,44 @@ public class Main {
         a.mult(ident);
         System.out.println(a.toString());
         */
+       
+       double[] pirateAttacks = new double[28];
+       double[] avgTemp = new double[28];
+       try {
+        Scanner sc = new Scanner(new File("pirate_attacks.csv"));
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            for (int i=0; i<28; i++) {
+                if (line.substring(0, 4).equals((i+1993)+"")) {
+                    pirateAttacks[i]+=1;
+                }
+            }
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+      try {
+        Scanner sc = new Scanner(new File("Average Temperature 1900-2023.csv"));
+        while (sc.hasNextLine()) {
+            String[] info = sc.nextLine().split(",");
+            for (int i=0; i<28; i++) {
+                try {
+                    if (Integer.parseInt(info[0])==i+1993) {
+                        avgTemp[i]=Double.parseDouble(info[1]);
+                    }
+                } catch (Exception ex){}
+            }
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+      /* for latex formatting table
+      for (int i=0; i<28; i++) {
+        System.out.println(avgTemp[i]+" & "+pirateAttacks[i]+" \\\\");
+      }*/
+      System.out.println("Pirate Attacks: "+Arrays.toString(pirateAttacks));
+      System.out.println("Average Temperature: "+Arrays.toString(avgTemp));
     }
 }
